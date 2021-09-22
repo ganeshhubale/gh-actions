45cd92fd86b7aa6475ac48d661c596854bf2cc9e
Ganesh Hubale <ganeshhuble03@gmail.com>
2021-09-22 14:19:51 +0530

    pre-commit changes
    Signed-off-by: Ganesh Hubale <ganeshhuble03@gmail.com>

--- ghaction/__init__.py
+++ ghaction/__init__.py
@@ -1,8 +1,10 @@
 def event():
     return "GitHub action event"
 
+
 def task():
     return "GitHub action task"
+
 
 def learn():
     return "Learning GH actions"
--- tests/test_ghactions.py
+++ tests/test_ghactions.py
@@ -1,10 +1,10 @@
 from ghaction import event
 from ghaction import task
-from ghaction import learn
+
 
 def test_event():
     assert event() == "GitHub action event"
 
+
 def test_task():
     assert task() == "GitHub action task"
-
